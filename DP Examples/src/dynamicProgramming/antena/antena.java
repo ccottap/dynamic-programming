d48a330bf851e4951e5c708fb43f70ea717c8941
Carlos Cotta <4223157+ccottap@users.noreply.github.com>
2019-01-01 11:20:14 +0100

    Improving the documentation of the antenna problem

--- DP Examples/src/dynamicProgramming/antena/antena.java
+++ DP Examples/src/dynamicProgramming/antena/antena.java
@@ -1,5 +1,7 @@
 
 package dynamicProgramming.antena;
+
+
 /**
  * Resolución del problema de la antena de telefonía
  * Ejercicio 12, Sección 3.8, pp. 82-90
@@ -12,9 +14,9 @@ public class antena {
 	 * @param args antenna height (optional)
 	 */
 	public static void main(String[] args) {
-		int[] peso = {2, 3, 5, 7, 11};
+		int[] peso = {2, 3, 5, 7, 11};		// peso de cada tipo de bloque
 		int[] altura = {7, 15, 23, 29, 31}; // bloques ordenados por altura
-		int M = (args.length==0)?51:Integer.parseInt(args[0]);
+		int M = (args.length==0)?51:Integer.parseInt(args[0]); // altura de la antena
 		int n = peso.length;
 		
 		System.out.println(n + " tipos de bloques");
@@ -53,20 +55,20 @@ public class antena {
 	}
 	
 	/**
-	 * Devuelve el peso mínimo de la antena y el número de bloques de cada tipo.
-	 * @param M altura de la antena
-	 * @param peso peso de cada tipo de bloque
-	 * @param altura altura de cada bloque
-	 * @param sol el número de bloques de cada tipo (parámetro de salida)
-	 * @return el peso mínimo de la antena
+	 * Computes the optimal weight of the antenna and the number of blocks of each type.
+	 * @param M height of the antenna
+	 * @param p weight of each block type
+	 * @param a height of each block type
+	 * @param sol number of blocks of each type (output parameter)
+	 * @return optimal weight of the antenna
 	 */
-	public static double AntenaPesoMinimo (int M, int[] peso, int[] altura, int[] sol)
+	public static double AntenaPesoMinimo (int M, int[] p, int[] a, int[] sol)
 	{
-		int n = peso.length;
+		int n = p.length;
 		double[][] tabla = new double[n][M+1];
 		
-		RellenaTablaAntena (tabla, M, n, peso, altura);
-		ReconstruyeSolucion (tabla, M, n, altura, sol);
+		RellenaTablaAntena (tabla, M, n, p, a);
+		ReconstruyeSolucion (tabla, M, n, a, sol);
 		
 		System.out.println("Matriz de costes");
 		System.out.print(" \t|");
@@ -78,7 +80,7 @@ public class antena {
 		for (int i=0; i<n; i++) {
 			System.out.print("\n"+i+"\t|");
 			for (int j=0; j<=M; j++)
-				if (tabla[i][j] == inf)
+				if (tabla[i][j] == Infinity)
 					System.out.print("\too");
 				else
 					System.out.print("\t"+tabla[i][j]);
@@ -89,31 +91,52 @@ public class antena {
 				
 	}
 	
-	static double inf = Double.POSITIVE_INFINITY;
-	
+	/**
+	 * Infinity is used as the cost of subproblems w/o feasible solution
+	 */
+	private static final double Infinity = Double.POSITIVE_INFINITY;
+	/**
+	 * Fills the table with the optimal solution to each subproblem
+	 * @param tabla the table of optimal solutions (output parameter)
+	 * @param m the height of the antenna
+	 * @param n the number of block types
+	 * @param p the weight of each block type
+	 * @param a the height of each block type
+	 * @return the optimal weight of the antenna
+	 */
 	private static double RellenaTablaAntena(double[][] tabla, int m, int n, int[] p, int[] a) {
+		// tabla[i][j] = optimal weight of an antenna of height j
+		// built with block types i..n-1
+		
 		for (int i=0; i<n; i++)
-			tabla[i][0] = 0;
+			tabla[i][0] = 0;							// height = 0 -> weight = 0
 		
 		for (int i=n-1; i>=0; i--)
 			for (int j=1; j<=m; j++) 
 				if (a[i]>j) {
-					if (i==(n-1))
-						tabla[i][j] = inf;
+					if (i==(n-1))						
+						tabla[i][j] = Infinity;			// no more block types -> no solution
 					else
-						tabla[i][j] = tabla[i+1][j];
+						tabla[i][j] = tabla[i+1][j];	// block type i is skipped
 				} else {
 					if (i==(n-1))
-						tabla[i][j] = tabla[i][j-a[i]] + p[i];
+						tabla[i][j] = tabla[i][j-a[i]] + p[i]; // block type i is used
 					else
-						tabla[i][j] = Math.min(tabla[i+1][j], tabla[i][j-a[i]] + p[i]);
+						tabla[i][j] = Math.min(tabla[i+1][j], tabla[i][j-a[i]] + p[i]); // a choice is made
 				}
 		
 		return tabla[0][m];
 		
 	}
 	
-	
+	/**
+	 * Reconstruct the optimal solution given the table computed in {@link RellenaTablaAntena}
+	 * @param tabla the table of optimal solutions
+	 * @param m the height of the antenna
+	 * @param n the number of block types
+	 * @param a the height of each block type
+	 * @param sol the number of copies of each block type used (output parameter)
+	 */
 	private static void ReconstruyeSolucion(double[][] tabla, int m, int n, int[] a, int[] sol) {
 		int i,j;
 		
@@ -121,7 +144,7 @@ public class antena {
 			sol[i] = 0;
 		
 		i = 0; j = m;
-		if (tabla[i][j] != inf) {
+		if (tabla[i][j] != Infinity) {
 			while (j>0) {
 				if ((i==(n-1)) || (tabla[i][j] != tabla[i+1][j])) {
 					sol[i]++;
@@ -135,21 +158,21 @@ public class antena {
 
 
 	/**
-	 * Devuelve el peso mínimo de la antena y el número de bloques de cada tipo.
-	 * @param M altura de la antena
-	 * @param peso peso de cada tipo de bloque
-	 * @param altura altura de cada bloque
-	 * @param sol el número de bloques de cada tipo (parámetro de salida)
-	 * @return el peso mínimo de la antena
+	 * Computes the optimal weight of the antenna and the number of blocks of each type.
+	 * @param M height of the antenna
+	 * @param p weight of each block type
+	 * @param a height of each block type
+	 * @param sol number of blocks of each type (output parameter)
+	 * @return optimal weight of the antenna
 	 */
-	public static double AntenaPesoMinimo2 (int M, int[] peso, int[] altura, int[] sol)
+	public static double AntenaPesoMinimo2 (int M, int[] p, int[] a, int[] sol)
 	{
-		int n = peso.length;
+		int n = p.length;
 		double[] tabla = new double[M+1];
 		int[] decision = new int[M+1];
 		
-		RellenaTablaAntena2 (tabla, decision, M, n, peso, altura);
-		ReconstruyeSolucion2 (decision, M, n, altura, sol);
+		RellenaTablaAntena2 (tabla, decision, M, n, p, a);
+		ReconstruyeSolucion2 (decision, M, n, a, sol);
 
 		System.out.println("Matriz de costes: ");
 		for (int j=0; j<=M; j++)
@@ -159,7 +182,7 @@ public class antena {
 			System.out.print("\t--");
 		System.out.println();
 		for (int i=0; i<=M; i++)
-			if (tabla[i] == inf)
+			if (tabla[i] == Infinity)
 				System.out.print("\too");
 			else
 				System.out.print("\t"+tabla[i]);
@@ -179,14 +202,28 @@ public class antena {
 				
 	}
 	
+	/**
+	 * Fills the table with the optimal solution to each subproblem
+	 * @param tabla the table of optimal solutions (output parameter)
+	 * @param decision table with the optimal decision associated to each subproblems (output parameter)
+	 * @param m the height of the antenna
+	 * @param n the number of block types
+	 * @param p the weight of each block type
+	 * @param a the height of each block type
+	 * @return the optimal weight of the antenna
+	 */
 	private static double RellenaTablaAntena2(double[] tabla, int[] decision, int m, int n, int[] p, int[] a) {
-		tabla[0] = 0;
-		decision[0] = -1;
+		
+		// tabla [j] = optimal weight of an antenna of height j using any block
+		// type with height a_i<=j
+		
+		tabla[0] = 0;		// no height -> no weight
+		decision[0] = -1;	// no block is used
 		
 		for (int j=1; j<=m; j++) {
-			tabla[j] = inf;
+			tabla[j] = Infinity;
 			decision[j] = -1;
-			for (int i=0; (i<n) && (a[i]<=j); i++) { // asume que los bloques están ordenados por altura
+			for (int i=0; (i<n) && (a[i]<=j); i++) { // blocks assumed to be sorted by height
 				double q = p[i] + tabla[j-a[i]];
 				if (q<tabla[j]) {
 					tabla[j] = q;
@@ -200,7 +237,14 @@ public class antena {
 		
 	}
 	
-	
+	/**
+	 * Reconstruct the optimal solution given the table computed in {@link RellenaTablaAntena2}
+	 * @param d the table of optimal decisions
+	 * @param m the height of the antenna
+	 * @param n the number of block types
+	 * @param a the height of each block type
+	 * @param sol the number of copies of each block type used (output parameter)
+	 */
 	private static void ReconstruyeSolucion2(int[] d, int m, int n, int[] a, int[] sol) {
 		int j;
 		
